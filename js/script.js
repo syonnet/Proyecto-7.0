859eeea3035419f6221ecd7ca9f0b750cd3f7b54
syonnet <vic_gen@hotmail.com>
2023-01-13 00:07:18 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+//Inicio del POuPvideo
+window.document.onkeydown = function (e) {
+  if (!e) {
+    e = event;
+  }
+  if (e.keyCode == 27) {
+    lightbox_close();
+  }
+};
+
+function lightbox_open() { //abrir la caja
+  var lightBoxVideo = document.getElementById("VideoCard");
+  window.scrollTo(0, 0);
+  document.getElementById("light").style.display = "block";
+  document.getElementById("fade").style.display = "block";
+  lightBoxVideo.play();
+}
+
+function lightbox_close() {
+  var lightBoxVideo = document.getElementById("VideoCard");
+  document.getElementById("light").style.display = "none";
+  document.getElementById("fade").style.display = "none";
+  lightBoxVideo.pause();
+}
+
+//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+function seleccionar(link) {
+  var opciones = document.querySelectorAll("#links  a");
+  opciones[0].className = "";
+  opciones[1].className = "";
+  opciones[2].className = "";
+  opciones[3].className = "";
+  opciones[4].className = "";
+  link.className = "seleccionado";
+
+  //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
+  //en modo responsive
+  var x = document.getElementById("nav");
+  x.className = "";
+}
+
+//función que muestra el menu responsive
+function responsiveMenu() {
+  var x = document.getElementById("nav");
+  if (x.className === "") {
+    x.className = "responsive";
+  } else {
+    x.className = "";
+  }
+}
+
+//detecto el scrolling para aplicar la animación del la barra de habilidades
+window.onscroll = function () {
+  efectoHabilidades();
+};
+
+//funcion que aplica la animación de la barra de habilidades
+function efectoHabilidades() {
+  var skills = document.getElementById("skills");
+  var distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+  if (distancia_skills >= 300) {
+    document.getElementById("nova").classList.add("barra-progreso1");
+    document.getElementById("cal").classList.add("barra-progreso2");
+    document.getElementById("cum").classList.add("barra-progreso3");
+    document.getElementById("ser").classList.add("barra-progreso4");
+  }
+}
